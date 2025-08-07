14cbeb347f7661de812564b07ce24133ea1f278b
Bunny2005-bolt <abhishekvenkata175@gmail.com>
2025-08-07 15:06:58 +0530

    Abhishek added substraction

--- Calculator.java
+++ Calculator.java
@@ -4,8 +4,8 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
-    public int sub(int a, int b) {
-        return a - b;
+    public int sub(int c, int d) {
+        return c - d;
     }
     // Main method to test the add method
     public static void main(String[] args) {
@@ -13,5 +13,8 @@ public class Calculator {
 
         int sum = calc.add(10, 20);
         System.out.println("The sum is: " + sum); // Output: The sum is: 30
+
+        int Diff = calc.sub(4, 12);
+        System.out.println("The Difference is: "+ Diff);
     }
 }
